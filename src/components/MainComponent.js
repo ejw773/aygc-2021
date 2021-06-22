9eaaf9af974e1ba8c191d9becc5e24517c719f8f
Elijah Wilcott <ejw773@gmail.com>
2021-06-22 14:48:49 -0400

    adds react-router-dom

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import Header from "./HeaderComponent"
 import Footer from './FooterComponent'
+import About from './AboutComponent';
+import Services from './ServicesComponent'
+import Home from './HomeComponent'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
 
 class Main extends React.Component {
     constructor(props) {
@@ -11,11 +22,37 @@ class Main extends React.Component {
     }
     render() {
         return (
-            <div>
-                <Header />
-                <Footer />
-            </div>
-        )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>        )
     }
 }
 
