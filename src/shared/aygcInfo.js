c9f9cf17abd3a608176e234171e1407362911f1d
Elijah Wilcott <ejw773@gmail.com>
2021-06-27 17:58:50 -0400

    moves data into shared/aygcInfo.js

---
+++ src/shared/aygcInfo.js
@@ -0,0 +1,22 @@
+export const featureData = [
+    {
+        featureIndex: 0,
+        featureIcon: "bi-globe",
+        featureTitle: "Global",
+        featureText: "AYGC, Inc. (American Yuncheng Gravure Cylinder, Inc.) is a part of the global Yuncheng Group, which includes 11,000 employees and 120+ plants spread over 30+ countries."
+    },
+    {
+        featureIndex: 1,
+        featureIcon: "bi-map",
+        featureTitle: "Local",
+        featureText: "In 2007, AYGC, Inc. established an operation in Spartanburg, South Carolina. Our state-of-the-art facility manufactures regular and speciality engraved printing cylinders."
+    },
+    {
+        featureIndex: 2,
+        featureIcon: "bi-graph-up",
+        featureTitle: "Growing",
+        featureText:"Our global buying power and in-house manufacturing of most of our equipment  makes us connected, innovative, and unrelentingly responsive to our client’s needs."
+    }
+];
+
+
