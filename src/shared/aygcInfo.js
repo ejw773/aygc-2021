e5a5fccd4e58f468349d4d512a5038cde6d804d3
Elijah Wilcott <ejw773@gmail.com>
2021-06-29 09:14:44 -0400

    adds data to aygcInfo

--- src/shared/aygcInfo.js
+++ src/shared/aygcInfo.js
@@ -20,3 +20,26 @@ export const featureData = [
 ];
 
 
+export const homeData = [
+    {
+        index: 0,
+        homeImage: '',
+        homeTitle: 'Global Footprint',
+        homeText: 'AYGC is a part of the global Yuncheng Group, which includes 11,000 employees and 120+ plants spread over 30+ countries.</br >Our global buying power, coupled with in-house manufacturing of most of our production equipment, has allowed us to be nimble, connected, innovative and, most of all, responsive to our client’s needs.'
+    },
+    {
+        index: 1,
+        homeImage: '',
+        homeTitle: 'Our Story',
+        homeText1: 'By any chosen measure, Yuncheng is growing rapidly.<br />In the early 1980s, a forward-looking lone ranger became fascinated with a supposedly “dead” technology: rotogravure printing. His former colleagues thought he was crazy to throw everything he had into this dream.<br />Fast-forward to today, and he is the owner of the largest rotogravure engraving company in the world.<br />You don’t create a massive global footprint by not doing a great job for our clients.'
+    }
+];
+
+export const contactData = [
+    {
+        index: 0,
+        contactImage: '',
+        contactTitle: 'A Town Named After a Revolutionary War Militia',
+        contactText: 'AYGC is located in the charming town of Spartanburg, South Carolina. The local area is known for BMW manufacturing, and as the corporate home of Denny’s. Spartanburg was formed in 1785 and was named after a local Revolutionary War militia unit, called the Spartan Regiment.<br />The downtown area, peppered with restaurants and cafes, preserves much of that old-time small-town feel.<br />Feel free to get in touch with us through the information below.'
+    }
+]
